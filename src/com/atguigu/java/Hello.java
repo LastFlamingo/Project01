5d84f4c87b029fad8b53b5ee05bcfe71975e5a22
dxd_glb <361601829@qq.com>
2020-04-20 21:45:44 +0800

    Initial commit

---
+++ src/com/atguigu/java/Hello.java
@@ -0,0 +1,20 @@
+package com.atguigu.java;
+
+import java.util.ArrayList;
+
+/**
+ * @author DXD
+ * @date 2020/4/15 22:13
+ */
+public class Hello {
+    public static final int NUM=0;
+    public static final String A="";
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        list.add(123);
+        list.add(456);
+        list.add(789);
+        if (list == null) {
+        }
+    }
+}
